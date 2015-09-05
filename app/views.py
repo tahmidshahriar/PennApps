64efa5446499669912c3f2d9bf9387b4782a6ec9
Arvind Raju <arvindraju@Arvinds-MacBook-Air.local>
2015-09-05 00:35:53 -0400

    fuck you tahmid

---
+++ app/views.py
@@ -0,0 +1,57 @@
+from flask import render_template, flash, redirect, request
+from app import app
+from .forms import LoginForm
+import pymongo
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user1= {'nickname': 'Arvind',
+    		'statement': 'Fuck you Tahmid'} 
+    user2= {'nickname': 'Tahmid',
+    		'statement': 'But Whyyyyyyyyy'} # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user1=user1,
+                           user2=user2)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+	form = LoginForm()
+	if form.validate_on_submit():
+		try:
+			conn=pymongo.MongoClient()
+			db = conn.pennapps
+			userInfo = db.userInfo
+			print userInfo
+			data = {}
+			data['username'] = request.form['username']
+			data['password'] = request.form['password']
+			userInfo.insert(data)
+			return redirect('/index')
+
+		except pymongo.errors.ConnectionFailure, e:
+			print "Could not connect to MongoDB: %s" % e
+			return redirect('/index')
+	else:
+		return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		try:
+			conn=pymongo.MongoClient()
+			db = conn.pennapps
+			userInfo = db.userInfo
+			usernameToVerify = userInfo.find({'username': request.form['username']})
+			if(request.form['password'] == usernameToVerify[0]['password']):
+				return redirect('www.google.com')
+
+
+		except pymongo.errors.ConnectionFailure, e:
+			print "Could not connect to MongoDB: %s" % e
+			return redirect('/index')
+	else:
+		return render_template('login.html', title='Sign In', form=form)
