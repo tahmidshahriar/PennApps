1c17ddff1b9e466bafdd3ba6f049ba825ac9bbc9
David Ongchoco <davidong@sas.upenn.edu>
2015-09-05 04:45:37 -0400

    stuff

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, request
 from app import app
-from .forms import LoginForm
+from .forms import LoginForm, LunchForm
 import pymongo
 
 
@@ -59,3 +59,51 @@ def login():
 			return redirect('/index')
 	else:
 		return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/addLunchRequest')
+def showAddWish():
+    return render_template('addWish.html')
+
+
+@app.route('/lunchPost', methods=['GET', 'POST'])
+def lunchPost():
+	form = LunchForm()
+	if form.validate_on_submit():
+		try:
+			conn=pymongo.MongoClient()
+			db = conn.pennapps
+			lunchDetails = db.lunchDetails
+			data = {}
+			data['title'] = request.form['title']
+			data['post'] = request.form['post']
+			lunchDetails.insert(data)
+			return redirect('/index')
+		except pymongo.errors.ConnectionFailure, e:
+			print "Could not connect to MongoDB: %s" % e
+			return redirect('/temp.html')
+	else:
+		return render_template('temp.html', title='ADD A WISH', form=form)
+
+
+@app.route('/newsFeedStuff', methods=['GET', 'POST'])
+def newsFeed():
+	try:
+		conn=pymongo.MongoClient()
+		db = conn.pennapps
+		lunchDetails = db.lunchDetails
+		myList = list(lunchDetails.find())
+		f = open('./templates/temporary.html','w')
+		message = """<html>
+		<head></head>
+		<body><p> s""" + myList[0]['title'] + """</p></body>
+		</html>"""
+		f.write(message)
+
+		f.close()
+		return render_template('news.html')	
+	except pymongo.errors.ConnectionFailure, e:
+		print "Could not connect to MongoDB: %s" % e
+		return redirect('/temp.html')
+
+
+
